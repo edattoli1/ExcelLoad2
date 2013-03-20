1318aafe6e8718df57b2f25d783129f76a131a91
Eric Dattoli <minsey@gmail.com>
2013-03-19 20:57:47 -0400

    4

--- ExcelLoad2/Form1.cs
+++ ExcelLoad2/Form1.cs
@@ -7,6 +7,8 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using DocumentFormat.OpenXml.Packaging;
+using DocumentFormat.OpenXml.Spreadsheet;
 
 namespace ExcelLoad2
 {
@@ -56,8 +58,8 @@ namespace ExcelLoad2
                 // Set the column header style.
                 DataGridViewCellStyle columnHeaderStyle = new DataGridViewCellStyle();
 
-                columnHeaderStyle.BackColor = Color.Beige;
-                columnHeaderStyle.Font = new Font("Verdana", 10, FontStyle.Bold);
+                columnHeaderStyle.BackColor = System.Drawing.Color.Beige;
+                columnHeaderStyle.Font = new System.Drawing.Font("Verdana", 10, FontStyle.Bold);
                 dataGridView1.ColumnHeadersDefaultCellStyle = columnHeaderStyle;
 
                 // Set the column header names.
@@ -91,5 +93,79 @@ namespace ExcelLoad2
                 }
             }
         }
+
+        public static string XLGetCellValue(string fileName, string sheetName, string addressName)
+        {
+            string value = null;
+
+            using (SpreadsheetDocument document = SpreadsheetDocument.Open(fileName, false))
+            {
+                WorkbookPart wbPart = document.WorkbookPart;
+
+                // Find the sheet with the supplied name, and then use that Sheet object
+                // to retrieve a reference to the appropriate worksheet.
+                Sheet theSheet = wbPart.Workbook.Descendants<Sheet>().
+                  Where(s => s.Name == sheetName).FirstOrDefault();
+
+                if (theSheet == null)
+                {
+                    throw new ArgumentException("sheetName");
+                }
+
+                // Retrieve a reference to the worksheet part, and then use its Worksheet property to get 
+                // a reference to the cell whose address matches the address you've supplied:
+                WorksheetPart wsPart = (WorksheetPart)(wbPart.GetPartById(theSheet.Id));
+                Cell theCell = wsPart.Worksheet.Descendants<Cell>().
+                  Where(c => c.CellReference == addressName).FirstOrDefault();
+
+                // If the cell doesn't exist, return an empty string:
+                if (theCell != null)
+                {
+                    //value = theCell.InnerText;
+
+                    /*  To retrieve just the value */
+                    value = theCell.CellValue.InnerText;
+
+
+                    // If the cell represents an integer number, you're done. 
+                    // For dates, this code returns the serialized value that 
+                    // represents the date. The code handles strings and booleans
+                    // individually. For shared strings, the code looks up the corresponding
+                    // value in the shared string table. For booleans, the code converts 
+                    // the value into t he words TRUE or FALSE.
+                    if (theCell.DataType != null)
+                    {
+                        switch (theCell.DataType.Value)
+                        {
+                            case CellValues.SharedString:
+                                // For shared strings, look up the value in the shared strings table.
+                                var stringTable = wbPart.GetPartsOfType<SharedStringTablePart>().FirstOrDefault();
+                                // If the shared string table is missing, something's wrong.
+                                // Just return the index that you found in the cell.
+                                // Otherwise, look up the correct text in the table.
+                                if (stringTable != null)
+                                {
+                                    value = stringTable.SharedStringTable.ElementAt(int.Parse(value)).InnerText;
+                                }
+                                break;
+
+                            case CellValues.Boolean:
+                                switch (value)
+                                {
+                                    case "0":
+                                        value = "FALSE";
+                                        break;
+                                    default:
+                                        value = "TRUE";
+                                        break;
+                                }
+                                break;
+                        }
+                    }
+                }
+            }
+            return value;
+        }
+
     }
 }
